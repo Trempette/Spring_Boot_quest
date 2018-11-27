f680ac221c3077a1948711585cb7f586e38164d6
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-11-27 20:42:23 +0100

    solution 2 en cours

--- src/main/java/fr/wildcodeschool/thewizardproject/models/Dumbledore.java
+++ src/main/java/fr/wildcodeschool/thewizardproject/models/Dumbledore.java
@@ -1,5 +1,8 @@
 package fr.wildcodeschool.thewizardproject.models;
 
+import org.springframework.stereotype.Component;
+
+@Component("Dumbledore")
 public class Dumbledore implements WizardInterface {
 	Outfit mOutfit;
 	
--- src/main/java/fr/wildcodeschool/thewizardproject/models/Outfit.java
+++ src/main/java/fr/wildcodeschool/thewizardproject/models/Outfit.java
@@ -1,12 +1,12 @@
 package fr.wildcodeschool.thewizardproject.models;
 
+import org.springframework.stereotype.Component;
 
 
 public class Outfit {
 	public Outfit() {
 
     }
-
 	
 	public String changeDress() {
 		return "the color of the dress is fucking red, but we already know neither Gandalf or Dumbldore got a red dress, this make fucking no sense.";
