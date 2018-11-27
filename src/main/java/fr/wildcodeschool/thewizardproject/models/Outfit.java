a33a75d9b2572ecfa7addca66f1166d9e0aeee74
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-11-27 14:13:57 +0100

    solution 1 final

--- src/main/java/fr/wildcodeschool/thewizardproject/models/Outfit.java
+++ src/main/java/fr/wildcodeschool/thewizardproject/models/Outfit.java
@@ -8,6 +8,6 @@ public class Outfit {
 
 	
 	public String changeDress() {
-		return "the color of the dress is fucking blue";
+		return "the color of the dress is fucking red, but we already know neither Gandalf or Dumbldore got a red dress, this make fucking no sense.";
 	}
 }
